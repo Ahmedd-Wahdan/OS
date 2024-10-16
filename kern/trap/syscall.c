ac00c18ef11f930a7f11bf1a2b70b9af8cc3124f
Ahmedd-Wahdan <ahmed.wahdan.work@gmail.com>
2024-10-16 15:53:42 +0300

    added comment

--- kern/trap/syscall.c
+++ kern/trap/syscall.c
@@ -328,7 +328,7 @@ void sys_allocate_user_mem(uint32 virtual_address, uint32 size)
 void sys_allocate_chunk(uint32 virtual_address, uint32 size, uint32 perms)
 {
 	if(virtual_address == 0 || virtual_address<USER_HEAP_START||virtual_address>=USER_HEAP_MAX||size<=0)
-			{
+			{//env_exit
 				env_exit();
 			}
 	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
